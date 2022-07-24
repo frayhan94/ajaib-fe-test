b0dece94fa4d35baa4cf77a1c760563c139d3b87
Faris Rayhan <frayhan94@gmail.com>
2022-07-24 10:02:31 +0700

    hotfix/home-user: remove persist because we do not need it at this moment

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import store from './app/store';
 
 import App from './app/App';
 import 'antd/dist/antd.css';
 import './styles/index.scss';
-
-const persistor = persistStore(store);
 
 const container = document.getElementById('root');
 
@@ -20,9 +16,7 @@ const root = createRoot(container);
 root.render(
   <Router>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </Router>,
 );
